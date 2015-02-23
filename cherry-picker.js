00b38d55a37ac29a1ea0e37f3c0e9e47735f4b9b
Robin Berjon <robin@berjon.com>
2015-02-23 15:05:03 +0100

    cherry picker knows when to run and when to update itself

--- cherry-picker.js
+++ cherry-picker.js
@@ -3,6 +3,7 @@
 var spork = require("./spork")
 ,   fs = require("fs-extra")
 ,   pth = require("path")
+,   exec = require("child_process").exec
 ,   jn = pth.join
 ,   nopt = require("nopt")
 ,   Octokit = require("octokit")
@@ -18,13 +19,15 @@ var spork = require("./spork")
     }
 ,   options = nopt(knownOpts, shortHands, process.argv, 2)
 ,   config = require(options.config)
-,   cacheFile = jn(config.outDir, "last-sha.txt")
+,   cacheFile = jn(config.outDir, "last-html-sha.txt")
 ;
 
-if (!fs.existsSync(cacheFile)) {
-    log.info("Creating missing cache file: " + cacheFile);
-    fs.writeFileSync(cacheFile, "42", "utf8");
+function nullifyCache () {
+    log.info("Creating/resetting cache file: " + cacheFile);
+    fs.writeFileSync(cacheFile, "this is not a sha", "utf8");
 }
+
+if (!fs.existsSync(cacheFile)) nullifyCache();
 
 // give spork a reporter; on error send email
 function run () {
@@ -38,14 +41,44 @@ if (options.force) return run();
 
 // check last commit
 var repo = gh.getRepo("whatwg", "html-mirror");
-repo.getCommits({})
+repo.getCommits()
     .then(function (commits) {
-        var lastRun = fs.readFileSync(cacheFile, "utf8");
-        log.info("Last processed commit: " + lastRun + ", last GH commit: " + commits[0]);
-        if (commits[0] !== lastRun) {
-            fs.writeFileSync(cacheFile, commits[0], "utf8");
+        var lastRun = fs.readFileSync(cacheFile, "utf8")
+        ,   lastCommit = commits[0].sha
+        ;
+        log.info("Last processed commit: " + lastRun + ", last GH commit: " + lastCommit);
+        if (lastCommit !== lastRun) {
+            fs.writeFileSync(cacheFile, lastCommit, "utf8");
             run();
+        }
+        else {
+            log.info("Nothing to report, going back to sleep.");
         }
     })
 ;
 
+// check for self-updates
+if (config.production) {
+    var repo = gh.getRepo("darobin", "spork");
+    repo.getCommits()
+        .then(function (commits) {
+            exec("git log -1", { cwd: __dirname }, function (err, stdout) {
+                if (err) log.error("Problem getting the git log: " + err);
+                var lastRun = stdout.replace(/^commit\s+/, "").replace(/\n[\s\S]+/, "")
+                ,   lastCommit = commits[0].sha
+                ;
+                log.info("Last fetched commit: " + lastRun + ", last GH commit: " + lastCommit);
+                if (lastCommit !== lastRun) {
+                    exec("git pull", { cwd: __dirname }, function (err) {
+                        if (err) log.error("Problem updating spork code: " + err);
+                        nullifyCache();
+                        log.info("Spork updated");
+                    });
+                }
+                else {
+                    log.info("Nothing to update.");
+                }
+            });
+        })
+    ;
+}
