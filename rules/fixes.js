954795ee0a2a2c786df1e978a1f70678f7c3f778
Robin Berjon <robin@berjon.com>
2015-02-23 20:43:33 +0100

    escape data URL properl

--- rules/fixes.js
+++ rules/fixes.js
@@ -6,4 +6,6 @@ exports.transform = function () {
     assert("Parsing overview image", $("img[src$='parsing-model-overview.svg']"))
         .attr("alt", "An overview of the parsing model.")
     ;
+    var $if = assert("Silly embedded HTML", $("iframe[src^='data:text/html']"));
+    $if.attr("src", $if.attr("src").replace(/&quot;/g, "%22"));
 };
