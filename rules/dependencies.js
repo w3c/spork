a9360eff10ce3518dc232a6f01852378511d1394
Robin Berjon <robin@berjon.com>
2015-02-23 13:56:40 +0100

    unslash absolute refs

--- rules/dependencies.js
+++ rules/dependencies.js
@@ -4,6 +4,9 @@ exports.name = "dependencies";
 exports.landscape = null;
 exports.transform = function () {
     window.unplugResources();
+    var unslash = function (str) {
+        return str.replace(/^\//, "");
+    };
     assert("Images, iframes, objects",
     $("img[src], iframe[src], object[data]"), "+").each(function () {
         var field = this.localName.toLowerCase() === "object" ? "data" : "src";
@@ -14,16 +17,16 @@ exports.transform = function () {
             if (url.hostname() === "resources.whatwg.org") return;
             if (url.hostname() === "code.jquery.com") return;
             if (url.hostname() === "html.spec.whatwg.org") {
-                window.info("Mapping " + this[field] + " to " + url.pathname());
-                this[field] = url.pathname(); // this just ensures that things stay relative
+                window.info("Mapping " + this[field] + " to " + unslash(url.pathname()));
+                this[field] = unslash(url.pathname()); // this just ensures that things stay relative
             }
             if (url.hostname() === "images.whatwg.org") {
                 window.info("Mapping " + this[field] + " to img" + url.pathname());
                 this[field] = "img" + url.pathname();
             }
             if (url.hostname() === "whatwg.org" && /^\/demos/i.test(url.pathname())) {
-                window.info("Mapping " + this[field] + " to " + url.pathname());
-                this[field] = url.pathname();
+                window.info("Mapping " + this[field] + " to " + unslash(url.pathname()));
+                this[field] = unslash(url.pathname());
             }
         }
     });
