6610048f90e1a47fc0353657181ba6ab806d0161
Robin Berjon <robin@berjon.com>
2015-02-27 15:27:32 +0100

    add the WD files

---
+++ profiles/html-wd.js
@@ -0,0 +1,4 @@
+
+module.exports = require("./html");
+module.exports.configuration.boilerplate = "res/boilerplate/html-wd-headers.html";
+module.exports.configuration.sotd = "res/boilerplate/html-wd-sotd.html";
