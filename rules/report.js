8b1c59896a758dd16e070541a3a54f5981b3233c
Robin Berjon <robin@berjon.com>
2015-03-03 17:30:43 +0100

    support splitting

--- rules/report.js
+++ rules/report.js
@@ -1,7 +1,109 @@
+/*globals assert*/
 
 exports.name = "report";
 exports.landscape = null;
-exports.transform = function () {
+exports.transform = function (data) {
     window.info("Sending source to be saved.");
-    window.saveSource("<!DOCTYPE html>\n" + document.documentElement.outerHTML);
+    window.saveSource("single-page.html", "<!DOCTYPE html>\n" + document.documentElement.outerHTML);
+    
+    // splitting happens here
+    var sections = [
+            "introduction"
+        ,   "infrastructure"
+        ,   "dom"
+        ,   "semantics"
+        ,   "editing"
+        ,   "browsers"
+        ,   "webappapis"
+        ,   "syntax"
+        ,   "the-xhtml-syntax"
+        ,   "rendering"
+        ,   "obsolete"
+        ,   "iana"
+        ,   "index"
+        ,   "references"
+        ,   "acknowledgments"
+        ]
+    ,   idMap = {}
+    ;
+    
+    // rewrite all links
+    sections.forEach(function (sec) {
+        assert("Section " + sec, 
+        $("#" + sec)
+            .parent())
+            .find("[id]")
+            .each(function () {
+                idMap["#" + $(this).attr("id")] = sec;
+            })
+        ;
+    });
+    assert("Links to IDs",
+    $("a[href^=#]")).each(function () {
+        var $a = $(this)
+        ,   id = $a.attr("href")
+        ;
+        $a.attr("href", idMap[id] + ".html" + id);
+    });
+    
+    // process all sections
+    sections.forEach(function (sec, idx) {
+        var doc = document.cloneNode(true)
+        ,   $h = assert("Section " + sec, $("#" + sec, doc))
+        ,   $sec = $h.parent()
+        ;
+        
+        // add lateral navigation plus link up to Toc (at bottom too)
+        var $nav = $("<nav class='prev_next'><a href='Overview.html#contents'>Table of contents</a></nav>", doc);
+        if (idx !== 0) {
+            $nav.prepend(" — ");
+            $("#toc-" + sections[idx - 1], doc)
+                .find("a:first")
+                .prepend("← ")
+                .clone()
+                .prependTo($nav)
+            ;
+        }
+        if (idx !== sections.length - 1) {
+            $nav.append(" — ");
+            $("#toc-" + sections[idx + 1], doc)
+                .find("a:first")
+                .append(" →")
+                .clone()
+                .appendTo($nav)
+            ;
+        }
+        
+        // keep only that section and ToC part
+        $sec.prevAll("section").each(function () {
+            var $s = $(this);
+            if ($s.find("> #contents").length) {
+                var $li = assert("Toc group for " + sec, $s.find("ul #toc-" + sec));
+                $li.prevAll("li").remove();
+                $li.nextAll("li").remove();
+                return;
+            }
+            $s.remove();
+        });
+        $sec.nextAll("section").remove();
+        
+        // drop all headers after the subtitle
+        assert("Parts of div.head not the title", $("div.head > :not(header)", doc), 2).remove();
+        
+        // set the title to the section title + the spec title
+        assert("There's a title", $("title")).text($h.text() + " | " + data.title);
+        
+        // save!
+        window.saveSource(sec + ".html", "<!DOCTYPE html>\n" + doc.documentElement.outerHTML);
+    });
+    
+    // save Overview too
+    var doc = document.cloneNode(true);
+    $("#contents", doc).nextAll("section").remove();
+    window.saveSource("Overview.html", "<!DOCTYPE html>\n" + doc.documentElement.outerHTML);
+};
+exports.params = function (conf) {
+    return [{
+        title:  conf.title || ""
+    }];
 };
